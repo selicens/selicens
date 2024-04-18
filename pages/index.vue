5d2fb8926291130f84b418ace51fc83725237f13
ZhouWei <1244620067@qq.com>
2024-04-18 23:49:59 +0800

    feat: close ssr

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,13 @@
 <script setup>
 defineOptions({ name: 'PageIndex' })
+useSeoMeta({
+  title: 'My Amazing Site',
+  ogTitle: 'My Amazing Site',
+  description: 'This is my amazing site, let me tell you all about it.',
+  ogDescription: 'This is my amazing site, let me tell you all about it.',
+  ogImage: 'https://example.com/image.png',
+  twitterCard: 'summary_large_image',
+})
 const route = useRoute()
 
 const open = ref(true)
