9a0f5a85623af35ceff85f0a237f7a04a726e00f
ZhouWei <1244620067@qq.com>
2024-04-27 22:54:34 +0800

    feat: add theme switch animation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,10 +3,10 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxt/ui'],
   app: {
-    baseURL: '/selicens/',
-    buildAssetsDir: '/assets/'
+    baseURL: '/selicens/', // 默认是 '/'，但GitHub Pages 默认部署到仓库同名路径下，所以一些文件会加载不到，因此需要改成仓库名
+    buildAssetsDir: '/assets/' // 默认是 '/_nuxt/'，但GitHub Pages 部署会忽略隐藏文件如'.'和'_'开头的文件，所以一些文件会加载不到，因此改成没有下划线开头的作为产物文件夹
   },
   experimental: {
-    payloadExtraction: false
+    payloadExtraction: false // 默认是 true，但GitHub Pages 部署会忽略隐藏文件如'.'和'_'开头的文件，他会生成_payload.json，因此改成 false
   }
 })
