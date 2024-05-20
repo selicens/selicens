e066869439be4d9c3a69f4870ccab12bbbce1543
ZhouWei <1244620067@qq.com>
2024-05-20 22:12:46 +0800

    feat: add css basic

---
+++ content/css-basic.md
@@ -0,0 +1,16 @@
+# CSS
+
+## box-sizing
+
+```css
+* {
+  width: 100px;
+  box-sizing: content-box;
+}
+// 默认值，边框与内边距都会增加到元素宽度中, 内容区 = 100px + border + padding
+* {
+  width: 100px;
+  box-sizing: border-box;
+}
+// 设置的边框和内边距的值是包含在 width 内的, 内容区 = 100px - (border + padding)
+```
--- pages/essay.vue
+++ pages/essay.vue
@@ -1,12 +1,20 @@
 <script setup>
 defineOptions({ name: 'Essay' })
+const router = useRouter()
 </script>
 
 <template>
   <UContainer>
     <div class="py-2">
       <UAlert
-        v-for="i in 10"
+        icon="i-heroicons-command-line"
+        description="css基础"
+        title="css"
+        class="my-2"
+        @click="() => router.push('/:css-basic')"
+      />
+      <UAlert
+        v-for="i in 9"
         icon="i-heroicons-command-line"
         description="You can add components to your app using the cli."
         title="Heads up!"
