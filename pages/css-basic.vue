3e48401887c0911b554e55196dcc8dbfc305188b
ZhouWei <1244620067@qq.com>
2024-05-21 23:46:31 +0800

    chore: adjusting content

---
+++ pages/css-basic.vue
@@ -0,0 +1,23 @@
+<script setup>
+defineOptions({ name: 'CssBasic' })
+</script>
+
+<template>
+  <UContainer>
+    <h1>box-sizing</h1>
+    <pre>
+      * {
+        width: 100px;
+        box-sizing: content-box;
+      }
+      // 默认值，边框与内边距都会增加到元素宽度中, 内容区 = 100px + border + padding
+      * {
+        width: 100px;
+        box-sizing: border-box;
+      }
+      // 设置的边框和内边距的值是包含在 width 内的, 内容区 = 100px - (border + padding)
+    </pre>
+  </UContainer>
+</template>
+
+<style scoped></style>
--- pages/essay.vue
+++ pages/essay.vue
@@ -11,14 +11,7 @@ const router = useRouter()
         description="css基础"
         title="css"
         class="my-2"
-        @click="() => router.push('/:css-basic')"
-      />
-      <UAlert
-        v-for="i in 9"
-        icon="i-heroicons-command-line"
-        description="You can add components to your app using the cli."
-        title="Heads up!"
-        class="my-2"
+        @click="() => router.push('/css-basic')"
       />
     </div>
   </UContainer>
